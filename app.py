60c152f159902d3dc47b156ff72c53828e449fc0
AntGreene <141074798+AntGreene@users.noreply.github.com>
2025-07-20 16:48:41 -0400

    added to contactUs

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, request, render_template, jsonify, session, redirect, url_for, flash;
 from flask_wtf import FlaskForm
+from flask_mail import Mail, Message
+import os
+"""Google birthday: 5/15/1985
+Harryscontactmail@gmail.com
+YnbdZ%fp52a_
 
+AP: lwyu xzma kbrh doro
+"""
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'keykeydoyouloveme'
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ['Mail_Username']
+app.config['MAIL_PASSWORD'] = os.environ['Mail_Password']
+
+mail = Mail(app)
 
 @app.route('/', methods=['GET', "POST"])
 @app.route('/home', methods=['GET', "POST"])
@@ -36,6 +51,27 @@ def about():
 
 @app.route('/contactUs', methods=['GET', "POST"])
 def contactUs():
+    if request.method == 'POST':
+        fName = request.form['fName']
+        lName = request.form['lName']
+        if request.form['phone']:
+            phone = request.form['phone']
+        email = request.form['email']
+        message = request.form['message']
+
+
+        msg = Message("New Contact Form Submission", 
+                      sender=os.environ['Mail_Username'],
+                      recipients=["harrysbodyshop@att.com"])
+        fullName = fName + " " + lName
+        msg.body = f"From: {fullName} <{email}>\n\n{message}"
+        try:
+            mail.send(msg)
+            flash("Message sent successfully!", "success")
+        except Exception as e:
+            flash(f"Failed to send message: {str(e)}", "error")
+        return redirect('contactUs')
+        
     return render_template('contactUs.html')
 
 @app.route('/success', methods=['GET', "POST"])
